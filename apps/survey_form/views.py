5f4333412f66385d7c52256da7ba6adc42fc0c6e
ehoversten <ehoversten@gmail.com>
2018-05-15 12:01:30 -0400

    added session count and removed commented code

--- apps/survey_form/views.py
+++ apps/survey_form/views.py
@@ -2,25 +2,10 @@ from django.shortcuts import render, HttpResponse, redirect
 
 # Create your views here.
 def index(request):
-    if 'name' not in request.session:
-        request.session['name'] = ''
-    if 'location' not in request.session:
-        request.session['location'] = ''
-    if 'fav' not in request.session:
-        request.session['fav'] = ''
-    if 'comments' not in request.session:
-        request.session['comments'] = ''
     if 'count' not in request.session:
-        request.session['count'] = 0
+        request.session['count'] = 1
 
-    context = {
-        'name' : '',
-        'location' : '',
-        'fav' : '',
-        'comments' : '',
-        'count' : ''
-    }
-    return render(request, 'survey_form/index.html', context)
+    return render(request, 'survey_form/index.html')
 
 def process(request):
     if request.method == 'POST':
@@ -40,5 +25,5 @@ def success(request):
     return render(request, 'survey_form/success.html')
 
 def reset(request):
-    request.session.clear()
+    # request.session.clear()
     return redirect('/')
